b7e488bca0b39891f23a56fc099f28a6ce5cd1c9
livee0322 <dlxodnd6564@naver.com>
2025-07-30 01:02:09 +0900

    nav.js 만들기

---
+++ nav.js
@@ -0,0 +1,23 @@
+// === 상단탭 ===
+document.write(`
+  <nav class="top-tabs">
+    <a href="/index.html">홈</a>
+    <a href="/clip.html">숏클립</a>
+    <a href="/event.html">이벤트</a>
+    <a href="/live.html">라이브</a>
+    <a href="/influencer.html">인플루언서</a>
+    <a href="/agency.html">대행서비스</a>
+    <a href="/news.html">뉴스</a>
+  </nav>
+`);
+
+// === 하단탭 ===
+document.write(`
+  <nav class="bottom-tab">
+    <a href="/index.html"><i class="ri-home-line"></i><div>홈</div></a>
+    <a href="/recruitlist.html"><i class="ri-live-line"></i><div>라이브</div></a>
+    <a href="/portfolio.html"><i class="ri-briefcase-line"></i><div>쇼호스트</div></a>
+    <a href="/liveschedule.html"><i class="ri-calendar-line"></i><div>일정</div></a>
+    <a href="/login.html" id="myTab"><i class="ri-user-3-line"></i><div>마이</div></a>
+  </nav>
+`);
