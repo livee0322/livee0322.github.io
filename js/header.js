69797fd2c25dffdcd7aa668acdf505c7970e4a34
livee0322 <dlxodnd6564@naver.com>
2025-07-30 14:34:01 +0900

    js/header.js 만들기

---
+++ js/header.js
@@ -0,0 +1,10 @@
+document.write(`
+  <header class="header">
+    <div class="logo" onclick="location.href='/index.html'">Livee</div>
+    <div class="right-icons">
+      <i class="ri-search-line"></i>
+      <i class="ri-notification-3-line"></i>
+      <i class="ri-user-line" onclick="location.href='/login.html'"></i>
+    </div>
+  </header>
+`);
