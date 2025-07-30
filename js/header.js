76321095bf07dae0da08277b3c0f27bf187656d7
livee0322 <dlxodnd6564@naver.com>
2025-07-30 15:19:18 +0900

    header.js 업데이트

--- js/header.js
+++ js/header.js
@@ -1,10 +1,17 @@
-document.write(`
-  <header class="header">
-    <div class="logo" onclick="location.href='/index.html'">Livee</div>
-    <div class="right-icons">
-      <i class="ri-search-line"></i>
-      <i class="ri-notification-3-line"></i>
-      <i class="ri-user-line" onclick="location.href='/login.html'"></i>
-    </div>
-  </header>
-`);
+document.addEventListener("DOMContentLoaded", () => {
+  const isLoggedIn = localStorage.getItem("loginUser");
+  const headerEl = document.getElementById("header-container");
+  if (!headerEl) return;
+
+  headerEl.innerHTML = `
+    <header class="header">
+      <a href="/index.html" class="logo">Livee</a>
+      <div class="right-icons">
+        <span id="welcomeMsg">
+          ${isLoggedIn ? `${JSON.parse(isLoggedIn).name}님` : ''}
+        </span>
+        <i class="ri-notification-line"></i>
+      </div>
+    </header>
+  `;
+});
